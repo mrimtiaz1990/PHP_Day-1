3d1ad4c5f1bf7237d3d7d5ad9b2b0f73612dda9c
Md. Imtiazul Islam <97890934+mrimtiaz1990@users.noreply.github.com>
2022-01-17 20:30:35 +0600

    Create Random_value.php

---
+++ Random_value.php
@@ -0,0 +1,16 @@
+<?php
+
+// Generate Random value Infinity (rand)
+echo "Generate Random value Infinity:", "<br>";
+
+$randomvalue = rand();
+echo ($randomvalue);
+echo "<br><br>";
+
+// Generate Random value in range (rand)
+echo "Generate Random value in range (-10, 10):", "<br>";
+
+$randvalue = rand(-10, 10);
+echo ($randvalue);
+
+?>
