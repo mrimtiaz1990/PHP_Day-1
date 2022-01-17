eb3179f37d2bdc4fedabf551be6b3d824f5cd5cb
Md. Imtiazul Islam <97890934+mrimtiaz1990@users.noreply.github.com>
2022-01-17 20:21:02 +0600

    Create Operator.php

---
+++ Operator.php
@@ -0,0 +1,18 @@
+<?php
+
+// Operator (+,-,*,/)
+echo "Operator (+,-,*,/):","<br>";
+
+$num1 = 13;
+$num2 = 3;
+
+echo "Here, Num-1 = ",$num1," & <br> Num-2 = ", $num2, "<br><br>";
+
+echo "Sum (+) = ", $num1 + $num2, "<br>";
+echo "Minous (-) = ", $num1 - $num2, "<br>";
+echo "Multiply (x) = ", $num1 * $num2, "<br>";
+echo "Division (/) = ", $num1 / $num2, "<br>";
+echo "Exponentiation (x*) = ", $num1 ** $num2, "<br>";
+echo "Modulus (%) = ", $num1 % $num2;
+
+?>
