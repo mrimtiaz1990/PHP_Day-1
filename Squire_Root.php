28055ae1c6ce0f521f8ae088cac395599a68e22f
Md. Imtiazul Islam <97890934+mrimtiaz1990@users.noreply.github.com>
2022-01-17 20:24:30 +0600

    Create Squire_Root.php

---
+++ Squire_Root.php
@@ -0,0 +1,13 @@
+<?php
+
+//Find Squire Root
+echo "Find Squire Root:", "<br>";
+
+$squire_value = 15376;
+$root_value = sqrt($squire_value);
+
+echo "Here, Value = ", $squire_value, "<br>";
+echo "Method 1: ", $root_value, "<br>";
+echo "Method 2: ", sqrt($squire_value);
+
+?>
