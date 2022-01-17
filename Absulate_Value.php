c07444a44f29bbcd66991dc14893613667d6e302
Md. Imtiazul Islam <97890934+mrimtiaz1990@users.noreply.github.com>
2022-01-17 20:29:40 +0600

    Create Absulate_Value.php

---
+++ Absulate_Value.php
@@ -0,0 +1,10 @@
+<?php
+
+// Convert absulate value (abs)
+echo "Convert absulate value:", "<br>";
+$Value1 = -102.6;
+
+echo "Here, Value = ", $Value1, "<br>";
+echo "Absulate Value = ", abs($Value1);
+
+?>
