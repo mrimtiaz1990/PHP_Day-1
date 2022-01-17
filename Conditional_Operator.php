c1fa5e0b85610f1f9fb372968f5f4500258488ac
Md. Imtiazul Islam <97890934+mrimtiaz1990@users.noreply.github.com>
2022-01-17 20:23:28 +0600

    Create Conditional_Operator.php

---
+++ Conditional_Operator.php
@@ -0,0 +1,17 @@
+<?php
+
+//Conditional Operator (<,>,<=,>=,==,!=)
+echo "Conditional Operator (<,>,<=,>=,==,!=):", "<br>";
+
+$num1 = 13;
+$num2 = 3;
+
+echo "Here, Num-1 = ", $num1, " & <br> Num-2 = ", $num2, "<br><br>";
+echo "Less Than (<) = ", $num1 < $num2, "<br>";
+echo "Greater Than (>) = ", $num1 > $num2, "<br>";
+echo "Less Than Equal (<=) = ", $num1 <= $num2, "<br>";
+echo "Greater Than Equal (>=) = ", $num1 >= $num2, "<br>";
+echo "Equal (=) = ", $num1 == $num2, "<br>";
+echo "Not Equal (!=) = ", $num1 != $num2;
+
+?>
