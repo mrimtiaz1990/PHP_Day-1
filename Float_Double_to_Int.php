c4b2006145296e5edccd31be5b36c98c284e979b
Md. Imtiazul Islam <97890934+mrimtiaz1990@users.noreply.github.com>
2022-01-17 20:25:47 +0600

    Create Float_Double_to_Int.php

---
+++ Float_Double_to_Int.php
@@ -0,0 +1,13 @@
+<?php
+
+// Convert Float/Double to Int value (int)
+echo "Convert Float/Double to Int value:", "<br>";
+
+$f_value = 97.8288234;
+$i_value = (int)$f_value;
+
+echo "Here, Value = ", $f_value, "<br>";
+echo "Method 1: ", $i_value, "<br>";
+echo "Method 2: ", (int)$f_value;
+
+?>
