2909987b2abd36a74995492aa8fccfd7a586d4a4
Md. Imtiazul Islam <97890934+mrimtiaz1990@users.noreply.github.com>
2022-01-26 13:58:49 +0600

    Add files via upload

---
+++ Stringto_LowercaseUppercase.php
@@ -0,0 +1,11 @@
+<?php
+
+$myname = "Md. Imtiazul Islam";
+
+echo "String to Upper Case: ";
+echo strtoupper($myname); // String to Upper Case
+echo "<br>";
+echo "String to Lower Case: ";
+echo strtolower($myname); // String to Lower Case
+
+?>
